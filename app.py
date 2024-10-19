82b8d87d1aab561d5b857a742a8f45860d63993d
Abhishek G <abhishekganesh2002@gmail.com>
2024-10-19 21:16:24 +0530

    Third commit

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ def load_image(image_file):
     img = Image.open(image_file)
     return img
 
+def get_image_pixel(filename):
+    with Image.open(filename) as rgb_image:
+        image_pixel = rgb_image.getpixel((30,30))
+    return image_pixel
+
 def main():
     st.title("Image Color Analyzer")
     menu = ["Home", "About"]
@@ -28,6 +33,10 @@ def main():
             img = load_image(image_file)
             st.image(img)
 
+            #Analysis
+            #Image Pixels
+            image_pixel = get_image_pixel(image_file)
+            st.write(image_pixel)
     else:
         st.subheader("About")
 
