2193c530911eefa58ed6d0417c2fd7dd7d4b8eb1
Abhishek G <abhishekganesh2002@gmail.com>
2024-10-19 20:36:01 +0530

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 def main():
     st.title("Image Color Analyzer")
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
 
 if __name__ == '__main__':
     main()
