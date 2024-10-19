1e24550e84c0f294538c23243c1d0e04001963d0
Abhishek G <abhishekganesh2002@gmail.com>
2024-10-19 21:03:21 +0530

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+from PIL import Image
+import altair as alt
+import pandas as pd
+import plotly.express as px
+
+from collections import Counter
+from sklearn.cluster import KMeans
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
 
 def main():
     st.title("Image Color Analyzer")
@@ -9,7 +24,10 @@ def main():
         st.subheader("Home")
 
         image_file = st.file_uploader("Upload Image", type = ['PNG', 'JPG', 'JPEG'])
-        
+        if image_file is not None:
+            img = load_image(image_file)
+            st.image(img)
+
     else:
         st.subheader("About")
 
