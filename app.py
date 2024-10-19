c05035eb58b33fd282fb339bf299e996a46c1079
Abhishek G <abhishekganesh2002@gmail.com>
2024-10-19 20:38:11 +0530

    second commit

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ def main():
     menu = ["Home", "About"]
     choice = st.sidebar.selectbox("Menu", menu)
 
+    if choice == "Home":
+        st.subheader("Home")
+
+        image_file = st.file_uploader("Upload Image", type = ['PNG', 'JPG', 'JPEG'])
+        
+    else:
+        st.subheader("About")
+
 if __name__ == '__main__':
     main()
